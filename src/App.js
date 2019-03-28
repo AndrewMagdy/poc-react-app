864dfe12f51d566428fd1f9858f87595ae93b09b
Andrew <andrewtawf@gmail.com>
2019-03-28 17:33:02 +0200

    Fix store

--- src/App.js
+++ src/App.js
@@ -6,24 +6,25 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import configureStore from "./store/configureStore";
 
+const store = configureStore({});
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.store = configureStore(props.initialState || {});
+    //store = configureStore(props.initialState || {});
   }
   componentDidMount() {
     if (this.props.stateChange) {
-      this.store.subscribe(() => this.props.stateChange(this.store.getState()));
+      store.subscribe(() => this.props.stateChange(store.getState()));
     }
   }
 
   render() {
     return (
-      <Provider store={this.store}>
+      <Provider store={store}>
         <Router>
           <Route path="/" exact component={ItemsListContainer} />
-          <Route path="/dashboard" exact component={ItemsListContainer} />
+          <Route path="/dashboard" component={ItemsListContainer} />
           <Route path="/basket" exact component={BasketContainer} />
           <Route path="/items" component={ItemsListContainer} />
           <Route path="/detail/:x" component={BasketContainer} />
